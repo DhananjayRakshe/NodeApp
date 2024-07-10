0b20d2181f2fe427e7100a4ce113e81054113e24
DhananjayRakshe <dhananjayrakshe77@gmail.com>
2024-07-10 15:20:10 +0530

    Async methods(setTimeOut, Promises)

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
-console.log("Hello Node JS");
-console.log("Setting Up Git in mac");
+// const person = [
+//     {name: "Dhananjay"},
+//     {name: "Shubham"},
+// ]
+
+// for(let user of person){
+//     console.log(user.name);
+// } 
+
+// Async Code
+// Normal Async Operation
+// const fetchData = (callBack) => {
+//     setTimeout(() => {
+//         callBack("Dhananjay");
+//     }, 1500)
+// }
+
+// setTimeout(() => {
+//     console.log("Rakshe");
+//      fetchData (text => {
+//         console.log(text);
+//     })
+// }, 2000)
+
+// console.log("Hi");
+
+// Promise Async Operation
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Dhananjay");
+    }, 1500)   
+    })
+   return promise;
+}
+
+setTimeout(() => {
+    console.log("Timer is done!");
+     fetchData()
+     .then( data => {
+        console.log(data);
+        return fetchData();
+     })
+     .then( data2 => {
+        console.log(data2);
+     })
+}, 2000)
+
+console.log("Hi");
