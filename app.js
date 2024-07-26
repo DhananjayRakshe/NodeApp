7c099a57d7e2d7d0c9621b1b1789f9452d0f0f68
DhananjayRakshe <dhananjayrakshe77@gmail.com>
2024-07-27 03:41:05 +0530

    Familier with express

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"></input><button>Submit</button></form>')
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
 })
 
-app.use((req, res, next) => {
-    console.log('In another middleware');
-})
+app.use('/',(req, res, next) => {
+    res.send('<h1> Hello from express </h1>')
+});
 
-const server = http.createServer(app);
-
-server.listen(3000);
+const port = 3000;
+app.listen(port);
 
 console.log("Server started on localhost: ", 3000)
